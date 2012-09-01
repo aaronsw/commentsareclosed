6303dc6ad0d9e8e9e2b1e827898f72f99bbba334
Jay Goel <dev@jaygoel.com>
2012-09-01 18:21:43 -0400

    Add ability to create templates based on path

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, make_response, render_template
+from jinja2.exceptions import TemplateNotFound
 
 app = Flask(__name__)
 app.config['DEBUG'] = bool(os.environ.get('DEBUG'))
@@ -7,7 +8,10 @@ app.config['DEBUG'] = bool(os.environ.get('DEBUG'))
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    return render_template("index.html")
+    try:
+        return render_template(path + '/index.html', path=path)
+    except TemplateNotFound as ex:
+        return render_template("index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
